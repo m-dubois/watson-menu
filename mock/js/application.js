7840d7c4c35605148702cc8cc2a29368c52a6771
Matthieu Dubois <matthieu.dubois@gmail.com>
2017-09-26 17:37:44 +0200

    updated layout

--- mock/js/application.js
+++ mock/js/application.js
@@ -1,11 +1,24 @@
 $(document).ready(function () {
-   init();
+   displayOpening();
 });
 
-init = function(){
-	$("#find-a-menu").parent().on("click", hideRibbon);
-};
+displayOpening = function() {
+    var circle = $("#circle");
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    circle.animate({
+      "width":w+100+"px",
+      "height":w+100+"px",
+      "margin-left":-(w/2)-50+"px",
+      "margin-top":-(w/2)-50+"px"
+    }, 1000, function() {
+      circle.css({
+        "width":"100%",
+        "border-radius":"0px",
+        "margin":"0px",
+        "top":"0px",
+        "left":"0px"
+      });
+    })
+}
 
-hideRibbon = function(){
-	//$("#ribbon").hide();
-};
