be2015b73308af4b31179ab4bf4eeda76d564b63
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-11-08 22:47:35 +0100

    reboot

---
+++ backend/src/main/java/org/matt/watson/backend/WatsonApiApplication.java
@@ -0,0 +1,12 @@
+package org.matt.watson.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatsonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatsonApiApplication.class, args);
+	}
+}
