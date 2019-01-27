573af70f0a3ab52a44a623b36942b5a981a922db
Matthieu Dubois <matthieu.dubois@owood.fr>
2019-01-27 23:13:58 +0100

    fixed MealResource

--- backend/src/main/java/org/matt/watson/backend/infra/rest/resources/MealResource.java
+++ backend/src/main/java/org/matt/watson/backend/infra/rest/resources/MealResource.java
@@ -1,10 +1,14 @@
 package org.matt.watson.backend.infra.rest.resources;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class MealResource {
     private StarterResource starter;
     private MainCourseResource mainCourse;
