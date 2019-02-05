089e02144d8158605e9d7872eb51b03685aaef5b
Matthieu Dubois <matthieu.dubois@owood.fr>
2019-02-05 06:56:57 +0100

    cleaned code for MenuServiceImpl

--- backend/src/main/java/org/matt/watson/backend/infra/service/MenuServiceImpl.java
+++ backend/src/main/java/org/matt/watson/backend/infra/service/MenuServiceImpl.java
@@ -1,18 +1,17 @@
 package org.matt.watson.backend.infra.service;
 
+import java.util.Date;
+import java.util.Optional;
+
 import org.matt.watson.backend.domain.model.Menu;
 import org.matt.watson.backend.domain.repository.MenuRepository;
 import org.matt.watson.backend.domain.service.MenuService;
 import org.matt.watson.backend.infra.repository.MenuEntity;
 import org.matt.watson.backend.infra.repository.MenuEntityMapper;
-import org.matt.watson.backend.infra.rest.StarterController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.Optional;
 
 @Service
 public class MenuServiceImpl implements MenuService {
